98dc26b9dbda4456f0461a112d0da90fb3a693af
WuXuanhao Gabrile <102649994+18268064963@users.noreply.github.com>
2022-12-01 13:27:52 +0800

    Add files via upload

---
+++ orderonline.php
@@ -0,0 +1,313 @@
+<?php
+session_start();
+
+
+
+
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: http://localhost/1/login");
+    exit;
+}
+
+include 'DBController.php';
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+if(!empty($_GET["action"])) {
+switch($_GET["action"]) {
+	case "add":
+		if(!empty($_POST["quantity"])) {
+			$productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
+			$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));
+			
+			if(!empty($_SESSION["cart_item"])) {
+				if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
+					foreach($_SESSION["cart_item"] as $k => $v) {
+							if($productByCode[0]["code"] == $k) {
+								if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+									$_SESSION["cart_item"][$k]["quantity"] = 0;
+								}
+								$_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+							}
+					}
+				} else {
+					$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+				}
+			} else {
+				$_SESSION["cart_item"] = $itemArray;
+			}
+		}
+	break;
+	case "remove":
+		if(!empty($_SESSION["cart_item"])) {
+			foreach($_SESSION["cart_item"] as $k => $v) {
+					if($_GET["code"] == $k)
+						unset($_SESSION["cart_item"][$k]);				
+					if(empty($_SESSION["cart_item"]))
+						unset($_SESSION["cart_item"]);
+			}
+		}
+	break;
+	case "empty":
+		unset($_SESSION["cart_item"]);
+	break;	
+}
+}
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Gallery</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="style2.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet"
+    href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Curabitur Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+ <script src="js/jquery-1.11.1.min.js"></script>
+  <script src="js/bootstrap.js"> </script>
+  <!---- start-smoth-scrolling---->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+ <script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+			});
+		});
+	</script>
+<!---End-smoth-scrolling---->
+
+
+</head>
+<body>
+		<!-- start-header section-->
+			<div class="header">
+				<div class="container">
+				     <div class="top-header">
+						<div class="phone">
+							<ul>
+								<li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span></li>
+								<li><p>+4758 655 5947</p></li>
+							</ul>
+						</div>
+						<div class="logo">
+							<h1><a href="index.html">Luca's the bread</a></h1>
+						</div>
+						<div class="header-right">
+						<div class="login">
+						<a href="login.html">login</a>
+						</div>
+						<div class="cart box_1">
+							<a href="checkout.html">
+								<h3> <span class="simpleCart_total"> $0.00 </span> (<span id="simpleCart_quantity" class="simpleCart_quantity"> 0 </span> items)<img src="images/bag.png" alt=""></h3>
+							</a>	
+							<p><a href="javascript:;" class="simpleCart_empty">Empty cart</a></p>
+							<div class="clearfix"> </div>
+						</div>
+				</div>
+		
+						<div class="clearfix"></div>
+					</div>	
+				</div>
+			</div>
+		<!-- end-header section-->
+		<!--start-banner-->
+			<div class="banner ban1">
+				<div class="container">
+					<div class="top-menu">
+					<span class="menu"><img src="images/nav.png" alt=""/> </span>
+						<ul>
+							<li><a  href="index.html">home</a></li>
+							<li><a  href="about.html">about us</a></li>
+							<li><a  href="upload.html">Careers</a></li>
+							<li><a  href="orderonline.php" class="active">Order online</a></li>
+							<li><a  href="events.html">Contact Us</a></li>
+							<li><a  href="register.php">register</a></li>
+						</ul>
+						<!-- script for menu -->
+									
+							 <script>
+							 $("span.menu").click(function(){
+							 $(".top-menu ul").slideToggle("slow" , function(){
+							 });
+							 });
+							 </script>
+						<!-- //script for menu -->
+					</div>
+					</div>	
+			</div>
+		<!--end-banner-->
+			
+			<!--gallery-->
+			<a href="http://localhost/1/ex2/logout.php" class="button3">Sign Out of Your Account</a>
+			<a href="http://localhost/1/ex2/index.php" class="button3">Sign Out of Your Account</a>
+<div id="shopping-cart">
+<div class="txt-heading">Shopping Cart</div>
+
+<a id="btnEmpty" href="orderonline.php?action=empty">Empty Cart</a>
+<?php
+if(isset($_SESSION["cart_item"])){
+    $total_quantity = 0;
+    $total_price = 0;
+?>	
+<table class="tbl-cart" cellpadding="10" cellspacing="1">
+<tbody>
+<tr>
+<th style="text-align:left;">Name</th>
+<th style="text-align:left;">Code</th>
+<th style="text-align:right;" width="5%">Quantity</th>
+<th style="text-align:right;" width="10%">Unit Price</th>
+<th style="text-align:right;" width="10%">Price</th>
+<th style="text-align:center;" width="5%">Remove</th>
+</tr>	
+<?php		
+    foreach ($_SESSION["cart_item"] as $item){
+        $item_price = $item["quantity"]*$item["price"];
+		?>
+				<tr>
+				<td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
+				<td><?php echo $item["code"]; ?></td>
+				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+				<td style="text-align:center;"><a href="orderonline.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+				</tr>
+				<?php
+				$total_quantity += $item["quantity"];
+				$total_price += ($item["price"]*$item["quantity"]);
+		}
+		?>
+
+<tr>
+<td colspan="2" align="right">Total:</td>
+<td align="right"><?php echo $total_quantity; ?></td>
+<td align="right" colspan="2"><strong><?php echo "$ ".number_format($total_price, 2); ?></strong></td>
+<td></td>
+</tr>
+</tbody>
+</table>		
+  <?php
+} else {
+?>
+<div class="no-records">Your Cart is Empty</div>
+<?php 
+}
+?>
+</div>
+
+<div id="product-grid">
+	<div class="txt-heading">Products</div>
+	<?php
+	$product_array = $db_handle->runQuery("SELECT * FROM tblproduct ORDER BY id ASC");
+	if (!empty($product_array)) { 
+		foreach($product_array as $key=>$value){
+	?>
+	
+		<div class="image">
+			
+			<div class="image"><img src="<?php echo $product_array[$key]["image"]; ?>"/>
+			<button class="quick_look" data-id="<?php echo $product_array[$key]["id"] ; ?>">Quick Look</button>
+			</div>
+			<form method="post" action="orderonline.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
+			<div class="product-tile-footer">
+			<div class="product-title"><?php echo $product_array[$key]["name"]; ?></div>
+			<div class="product-price"><?php echo "$".$product_array[$key]["price"]; ?></div>
+			<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+			</div>
+			
+			</form>
+			
+		</div>
+	<?php
+		}
+	}
+	
+	?>
+</div>
+</div>	
+	<!--//gallery-->
+
+
+			<div class="specials-section">
+				<div class="container">
+					<div class="col-md-3 specials">
+					<h3>about</h3>
+					<p>Morbi pretium gravida justo nec ultrices. Ut et facilisis justo. Fusce ac turpis eros, vel molestie lectus.feugiat velit velit non turpis</p>
+				</div>
+				<div class="col-md-3 specials1">
+				<h3>specials</h3>
+				<ul>
+					<li><a href="#">New Listing Sign-Up</a></li>
+					<li><a href="#">Consectetur adipiscing</a></li>
+					<li><a href="#">Integer molestie lorem</a></li>
+					<li><a href="#">Facilisis in pretium nisl</a></li>
+				</ul>
+				</div>
+			<div class="col-md-3 specials1">
+			<h3>recipes</h3>
+			<ul>
+				<li><a href="#">Integer molestie lorem</a></li>
+				<li><a href="#">Integer molestie lorem</a></li>
+				<li><a href="#">Consectetur adipiscing</a></li>
+				<li><a href="#">Lorem ipsum dolor sit</a></li>
+			</ul>
+			</div>
+			<div class="col-md-3 specials1">
+			<h3>social</h3>
+			<ul>
+				<li><a href="#">facebook</a></li>
+				<li><a href="#">twitter</a></li>
+				<li><a href="#">google</a></li>
+				<li><a href="#">viemo</a></li>
+			</ul>
+			</div>
+			<div class="clearfix"></div>
+			</div>
+			</div>
+	</div>
+	<div class="footer-section">
+					<div class="container">
+						<div class="footer-top">
+						<p>Copyright &copy; 2015.Company name All rights reserved.<a target="_blank" href="http://sc.chinaz.com/moban/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a></p>
+					</div>
+					<script type="text/javascript">
+						$(document).ready(function() {
+							/*
+							var defaults = {
+					  			containerID: 'toTop', // fading element id
+								containerHoverID: 'toTopHover', // fading element hover id
+								scrollSpeed: 1200,
+								easingType: 'linear' 
+					 		};
+							*/
+							
+							$().UItoTop({ easingType: 'easeOutQuart' });
+							
+						});
+					</script>
+				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+					</div>
+					</div>
+
+
+	
+</body>
+</html>
+	
---
+++ register2.php
@@ -0,0 +1,288 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $address = $salary = $username = $password ="";
+$name_err = $address_err = $salary_err = $username_err =$password_err ="";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate address
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an address.";     
+    } else{
+        $address = $input_address;
+    }
+    
+    // Validate salary
+    $input_salary = trim($_POST["salary"]);
+    if(empty($input_salary)){
+        $salary_err = "Please enter the salary amount.";     
+    } elseif(!ctype_digit($input_salary)){
+        $salary_err = "Please enter a positive integer value.";
+    } else{
+        $salary = $input_salary;
+    }
+
+    // Validate username
+if(empty(trim($_POST["username"]))){
+    $username_err = "Please enter a username.";
+} elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+    $username_err = "Username can only contain letters, numbers, and underscores.";
+} else{
+    // Prepare a select statement
+    $sql = "SELECT id FROM employees WHERE username = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+        
+        // Set parameters
+        $param_username = trim($_POST["username"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            /* store result */
+            mysqli_stmt_store_result($stmt);
+            
+            if(mysqli_stmt_num_rows($stmt) == 1){
+                $username_err = "This username is already taken.";
+            } else{
+                $username = trim($_POST["username"]);
+            }
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+}
+
+// Validate confirm password
+if(empty(trim($_POST["password"]))){
+    $password_err = "Please confirm password.";     
+} else{
+    $password = trim($_POST["password"]);
+    if(empty($password_err) && ($password != $password)){
+        $password_err = "Password did not match.";
+    }
+}
+ 
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($address_err) && empty($salary_err)&& empty($username_err)&& empty($password_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO employees (name, address, salary, username, password) VALUES (?, ?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssiss", $param_name, $param_address, $param_salary, $param_username, $param_password);
+            
+            // Set parameters
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+            $param_username = $username;
+            $param_password = $password;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: register2.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Contact </title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Curabitur Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+ <script src="js/jquery-1.11.1.min.js"></script>
+  <script src="js/bootstrap.js"> </script>
+  <!---- start-smoth-scrolling---->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+ <script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+			});
+		});
+	</script>
+<!---End-smoth-scrolling---->
+
+</head>
+<body>
+		<!-- start-header section-->
+			<div class="header">
+				<div class="container">
+				     <div class="top-header">
+						<div class="phone">
+							<ul>
+								<li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span></li>
+								<li><p>+4758 655 5947</p></li>
+							</ul>
+						</div>
+						<div class="logo">
+							<h1><a href="index.html">Luca's the bread</a></h1>
+						</div>
+						<div class="header-right">
+						<div class="login">
+						<a href="login.html">login</a>
+						</div>
+						<div class="cart box_1">
+							<a href="checkout.html">
+								<h3> <span class="simpleCart_total"> $0.00 </span> (<span id="simpleCart_quantity" class="simpleCart_quantity"> 0 </span> items)<img src="images/bag.png" alt=""></h3>
+							</a>	
+							<p><a href="javascript:;" class="simpleCart_empty">Empty cart</a></p>
+							<div class="clearfix"> </div>
+						</div>
+				</div>
+		
+						<div class="clearfix"></div>
+					</div>	
+				</div>
+			</div>
+		<!-- end-header section-->
+		<!--start-banner-->
+			<div class="banner ban1">
+				<div class="container">
+					<div class="top-menu">
+					<span class="menu"><img src="images/nav.png" alt=""/> </span>
+						<ul>
+							<li><a  href="index.html">home</a></li>
+							<li><a  href="about.html">about us</a></li>
+							<li><a  href="upload.php">Careers</a></li>
+							<li><a  href="orderonline.php">Order online</a></li>
+							<li><a  href="events.html">Contact Us</a></li>
+							<li><a  href="contact.html" class="active">register</a></li>
+						</ul>
+						<!-- script for menu -->
+									
+							 <script>
+							 $("span.menu").click(function(){
+							 $(".top-menu ul").slideToggle("slow" , function(){
+							 });
+							 });
+							 </script>
+						<!-- //script for menu -->
+					</div>
+					</div>	
+			</div>
+		<!--end-banner-->
+		<div class="wrapper2">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="title">
+                    <h2 class="mt-5">Registration successful</h2>
+                    <a href="register.php" >Back to register</a>
+                    </div>   
+                </div>
+            </div>        
+        </div>
+    </div>
+
+			<!--contact-->
+			
+		<div class="specials-section">
+				<div class="container">
+					<div class="col-md-3 specials">
+					<h3>about</h3>
+					<p>Morbi pretium gravida justo nec ultrices. Ut et facilisis justo. Fusce ac turpis eros, vel molestie lectus.feugiat velit velit non turpis</p>
+				</div>
+				<div class="col-md-3 specials1">
+				<h3>specials</h3>
+				<ul>
+					<li><a href="#">New Listing Sign-Up</a></li>
+					<li><a href="#">Consectetur adipiscing</a></li>
+					<li><a href="#">Integer molestie lorem</a></li>
+					<li><a href="#">Facilisis in pretium nisl</a></li>
+				</ul>
+				</div>
+			<div class="col-md-3 specials1">
+			<h3>recipes</h3>
+			<ul>
+				<li><a href="#">Integer molestie lorem</a></li>
+				<li><a href="#">Integer molestie lorem</a></li>
+				<li><a href="#">Consectetur adipiscing</a></li>
+				<li><a href="#">Lorem ipsum dolor sit</a></li>
+			</ul>
+			</div>
+			<div class="col-md-3 specials1">
+			<h3>social</h3>
+			<ul>
+				<li><a href="#">facebook</a></li>
+				<li><a href="#">twitter</a></li>
+				<li><a href="#">google</a></li>
+				<li><a href="#">viemo</a></li>
+			</ul>
+			</div>
+			<div class="clearfix"></div>
+			</div>
+			</div>
+	</div>
+	<div class="footer-section">
+					<div class="container">
+						<div class="footer-top">
+						<p>Copyright &copy; 2015.Company name All rights reserved.<a target="_blank" href="http://sc.chinaz.com/moban/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a></p>
+					</div>
+					<script type="text/javascript">
+						$(document).ready(function() {
+							/*
+							var defaults = {
+					  			containerID: 'toTop', // fading element id
+								containerHoverID: 'toTopHover', // fading element hover id
+								scrollSpeed: 1200,
+								easingType: 'linear' 
+					 		};
+							*/
+							
+							$().UItoTop({ easingType: 'easeOutQuart' });
+							
+						});
+					</script>
+				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+					</div>
+					</div>
+
+
+	
+</body>
+</html>
+	
